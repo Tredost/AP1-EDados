27e5c52da2491f0890183488b8349d31b071526d
Joao Victor Franco da Costa <142028379+JoaoFrancoDAC@users.noreply.github.com>
2024-05-02 20:24:00 -0300

    megagamb main.go
    iarra

--- Código/main.go
+++ Código/main.go
@@ -4,67 +4,72 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
-	"github.com/gorilla/mux"
 	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Produto struct {
-	ID          int     `json:"id"`
-	Nome        string  `json:"nome"`
-	Descrição   string  `json:"descrição"`
-	Valor       float64 `json:"valor"`
+	ID        int     `json:"id"`
+	Nome      string  `json:"nome"`
+	Descrição string  `json:"descrição"`
+	Valor     float64 `json:"valor"`
 }
 
 type Pedido struct {
-	ID          int                    `json:"id"`
-	Delivery    bool                   `json:"delivery"`
-	Produtos    []QuantidadeProduto    `json:"produtos"`  
-	ValorTotal  float64                `json:"valor_total"`
+	ID         int                 `json:"id"`
+	Delivery   bool                `json:"delivery"`
+	Produtos   []QuantidadeProduto `json:"produtos"`
+	ValorTotal float64             `json:"valor_total"`
 }
 
 type QuantidadeProduto struct {
-	ID         int `json:"id"` 
+	ID         int `json:"id"`
 	Quantidade int `json:"quantidade"`
 }
 
 type Métricas struct {
-	TotalProdutos      int     `json:"total_produtos"`
-	PedidosEncerrados  int     `json:"pedidos_encerrados"`
-	PedidosAndamento   int     `json:"pedidos_andamento"`
-	FaturamentoTotal   float64 `json:"faturamento_total"`
+	TotalProdutos     int     `json:"total_produtos"`
+	PedidosEncerrados int     `json:"pedidos_encerrados"`
+	PedidosAndamento  int     `json:"pedidos_andamento"`
+	FaturamentoTotal  float64 `json:"faturamento_total"`
 }
 
-type ListaProdutos []Produto  // ver de fzr um sruct
+type ListaDeProdutos struct {
+	Produtos []Produto
+}
 
-type FilaPedidos []Pedido // ver de fzr um sruct
+type FilaDePedidos struct {
+	Pedidos []Pedido
+}
 
 var (
-	listaProdutos ListaProdutos
-	filaPedidos   FilaPedidos
-	produtoID     int = 1 
-	pedidoID      int = 1 
+	ListaProdutos ListaDeProdutos
+	FilaPedidos   FilaDePedidos
+	produtoID     int = 1
+	pedidoID      int = 1
 	métricas      Métricas
 	lojaAberta    bool
 )
 
 // recebe produto e coloca na lsita
-func (lp *ListaProdutos) AdicionarProduto(produto Produto) {
-	*lp = append(*lp, produto)
+func (lp *ListaDeProdutos) AdicionarProduto(produto Produto) {
+	lp.Produtos = append(lp.Produtos, produto)
 }
 
-// tira da lista pelo id 
-func (lp *ListaProdutos) RemoverProduto(id int) {
-	for i, produto := range *lp {
+// tira da lista pelo id
+func (lp *ListaDeProdutos) RemoverProduto(id int) {
+	for i, produto := range lp.Produtos {
 		if produto.ID == id {
-			*lp = append((*lp)[:i], (*lp)[i+1:]...)
+			lp.Produtos = append((lp.Produtos)[:i], (lp.Produtos)[i+1:]...)
 			break
 		}
 	}
 }
 
-func (lp ListaProdutos) BuscarProdutoByID(id int) (Produto, bool) {
-	for _, produto := range lp {
+func (lp ListaDeProdutos) BuscarProdutoByID(id int) (Produto, bool) {
+	for _, produto := range lp.Produtos {
 		if produto.ID == id {
 			return produto, true
 		}
@@ -72,41 +77,41 @@ func (lp ListaProdutos) BuscarProdutoByID(id int) (Produto, bool) {
 	return Produto{}, false
 }
 
-func (lp ListaProdutos) ListarProdutos() ListaProdutos {
-	return lp
+func (lp ListaDeProdutos) ListarProdutos() []Produto {
+	return lp.Produtos
 }
 
-func (fp *FilaPedidos) IncluirPedido(pedido Pedido) {
-	*fp = append(*fp, pedido)
+func (fp *FilaDePedidos) IncluirPedido(pedido Pedido) {
+	FilaPedidos.Pedidos = append(FilaPedidos.Pedidos, pedido)
 }
 
-func (fp FilaPedidos) PedidosEmAberto() FilaPedidos {
-	var pedidosAbertos FilaPedidos
-	for _, pedido := range fp {
-		if pedido.Delivery || len(pedido.Produtos) > 0 {  // mexer depois
-			pedidosAbertos = append(pedidosAbertos, pedido)
+func (fp *FilaDePedidos) PedidosEmAberto() FilaDePedidos {
+	var pedidosAbertos FilaDePedidos
+	for _, pedido := range fp.Pedidos {
+		if pedido.Delivery || len(pedido.Produtos) > 0 { // mexer depois
+			pedidosAbertos.Pedidos = append(pedidosAbertos.Pedidos, pedido)
 		}
 	}
 	return pedidosAbertos
 }
 
-func (fp *FilaPedidos) ExpedirPedido() {
-	if len(*fp) > 0 {
-		*fp = (*fp)[1:] // remove o primeiro pedido da fila
+func (fp *FilaDePedidos) ExpedirPedido() {
+	if len(fp.Pedidos) > 0 {
+		fp.Pedidos = fp.Pedidos[1:] // remove o primeiro pedido da fila
 		métricas.PedidosEncerrados++
 	}
 }
 
 func handleAdicionarProduto(w http.ResponseWriter, r *http.Request) {
-	var produto Produto
-	err := json.NewDecoder(r.Body).Decode(&produto)
+	var Produto Produto
+	err := json.NewDecoder(r.Body).Decode(&Produto)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	produto.ID = produtoID
+	Produto.ID = produtoID
 	produtoID++
-	listaProdutos.AdicionarProduto(produto)
+	ListaProdutos.AdicionarProduto(Produto)
 	w.WriteHeader(http.StatusCreated)
 }
 
@@ -118,7 +123,7 @@ func handleObterProduto(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "ID inválido", http.StatusBadRequest)
 		return
 	}
-	produto, encontrado := listaProdutos.BuscarProdutoByID(id)
+	produto, encontrado := ListaProdutos.BuscarProdutoByID(id)
 	if !encontrado {
 		http.Error(w, "Produto não encontrado", http.StatusNotFound)
 		return
@@ -134,12 +139,12 @@ func handleRemoverProduto(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "ID inválido", http.StatusBadRequest)
 		return
 	}
-	listaProdutos.RemoverProduto(id)
+	ListaProdutos.RemoverProduto(id)
 	w.WriteHeader(http.StatusNoContent)
 }
 
 func handleObterTodosProdutos(w http.ResponseWriter, r *http.Request) {
-	produtos := listaProdutos.ListarProdutos()
+	produtos := ListaProdutos.ListarProdutos()
 	if len(produtos) == 0 {
 		fmt.Fprintln(w, "Não há produtos cadastrados")
 		return
@@ -157,7 +162,7 @@ func handleIncluirPedido(w http.ResponseWriter, r *http.Request) {
 
 	var valorTotal float64
 	for _, qp := range pedido.Produtos {
-		produto, encontrado := listaProdutos.BuscarProdutoByID(qp.ID)
+		produto, encontrado := ListaProdutos.BuscarProdutoByID(qp.ID)
 		if !encontrado {
 			http.Error(w, fmt.Sprintf("Produto com ID %d não encontrado", qp.ID), http.StatusBadRequest)
 			return
@@ -168,18 +173,20 @@ func handleIncluirPedido(w http.ResponseWriter, r *http.Request) {
 	pedido.ID = pedidoID
 	pedidoID++
 
-	if pedido.Delivery { valorTotal += 10 }
+	if pedido.Delivery {
+		valorTotal += 10
+	}
 	pedido.ValorTotal = valorTotal
 
-	filaPedidos.IncluirPedido(pedido)
+	FilaPedidos.IncluirPedido(pedido)
 	métricas.FaturamentoTotal += pedido.ValorTotal
 
 	w.WriteHeader(http.StatusCreated)
 }
 
 func handleObterPedidosAtivos(w http.ResponseWriter, r *http.Request) {
-	pedidosAtivos := filaPedidos.PedidosEmAberto()
-	if len(pedidosAtivos) == 0 {
+	pedidosAtivos := FilaPedidos.PedidosEmAberto()
+	if len(pedidosAtivos.Pedidos) == 0 {
 		fmt.Fprintln(w, "Não há pedidos ativos")
 		return
 	}
@@ -218,15 +225,15 @@ func main() {
 
 func expedirPedidos() {
 	for lojaAberta {
-		pedidosAtivos := filaPedidos.PedidosEmAberto()
-		if len(pedidosAtivos) > 0 {
+		pedidosAtivos := FilaPedidos.PedidosEmAberto()
+		if len(pedidosAtivos.Pedidos) > 0 {
 			time.Sleep(30 * time.Second)
-			filaPedidos.ExpedirPedido()
+			FilaPedidos.ExpedirPedido()
 		}
 	}
 }
 
 func atualizarMétricas() {
-	métricas.TotalProdutos = len(listaProdutos)
-	métricas.PedidosAndamento = len(filaPedidos)
+	métricas.TotalProdutos = len(ListaProdutos.Produtos)
+	métricas.PedidosAndamento = len(FilaPedidos.Pedidos)
 }
